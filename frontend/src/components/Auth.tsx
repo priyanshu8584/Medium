3d87021f65de36299a2b9be81185923f9a05fea0
Priyanshu <singh.priyanshu.1024@gmail.com>
2024-08-24 21:25:22 +0530

    changed /signup to /

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -56,7 +56,7 @@ export const Auth = ({type}:{type:"signup"|"signin"}) => {
           <button onClick={sendRequest} type="button" className="w-full mt-10 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type==="signup"?"Sign up":"Sign in"}</button>
           <div className="text-slate-400 pl-4 pt-8">
                  {type==="signup"?"Already have an account ":"Dont have an Account"}
-                  <Link  className="underline"to={type==="signup"?"/signin":"/signup"}>{type==="signup"?"Login":"Create an Account"}</Link>
+                  <Link  className="underline"to={type==="signup"?"/signin":"/"}>{type==="signup"?"Login":"Create an Account"}</Link>
             </div>
           </div>
            
