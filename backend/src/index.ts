8e0c2c9bfef5b0c76a8cace96b7576a717385ac3
Priyanshu <singh.priyanshu.1024@gmail.com>
2024-08-24 12:25:04 +0530

    changes in backend/package.json3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+
 const app = new Hono<{
   Bindings:{
     DATABASE_URL:string,
@@ -12,13 +13,12 @@ const app = new Hono<{
     
   }
 }>()
-app.use('/*', cors())
+
 app.route("/api/v1/user",userRouter);
 app.route("/api/v1/blog",blogRouter);
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
 
 //postgres://avnadmin:AVNS_vEJ61fdeMBdwwJgOfuB@pg-7ee75aa-singh-2f2c.a.aivencloud.com:14685/defaultdb?sslmode=require
 //DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZjAyYzY0M2UtMzBmNS00MGJkLTg3MmMtMTJhYzk5MTc3OGRjIiwidGVuYW50X2lkIjoiNDExOTczNWJjODA5MzcwOTQ1MTc5MjU4NWE3ZWI5MTk1OTI0NDg3OGJkOTU4ZjYyOThjMWQ0ZTc0MzQxODQ1ZiIsImludGVybmFsX3NlY3JldCI6IjNlZTYxYjcwLWE0Y2ItNDhmYS05NDNhLWVhNDc1NzAxOTQxZiJ9.59g2N_yAK1DJrtlEJ25MfMdwiCq_L1VX6O9zwgW0q3k"
